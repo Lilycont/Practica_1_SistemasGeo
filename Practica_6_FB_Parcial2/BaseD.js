ffc7f0b2b46411faaa8399a71d33a7047b5e32e2
Lilycont <Lilycont47@gmail.com>
2021-05-01 19:23:06 -0500

    practica 13 firebase

---
+++ Practica_6_FB_Parcial2/BaseD.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBs8F4b-r5t8P0_It3lTN5c6Kc60r6NtuY",
+    authDomain: "practica-geo-2.firebaseapp.com",
+    projectId: "practica-geo-2",
+    storageBucket: "practica-geo-2.appspot.com",
+    messagingSenderId: "368563117661",
+    appId: "1:368563117661:web:dfd988443f46e2641b77b4"
+  };
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
